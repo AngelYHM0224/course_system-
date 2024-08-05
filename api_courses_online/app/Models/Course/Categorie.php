386f40f2f325b9c43ec31743eb1541989563e80f
AngelYHM0224 <yaelhdz0510@gmail.com>
2024-08-05 08:59:14 -0600

    Ya enviea el registro de las categorias

--- api_courses_online/app/Models/Course/Categorie.php
+++ api_courses_online/app/Models/Course/Categorie.php
@@ -20,13 +20,13 @@ class Categorie extends Model
 
     public function setCreatedAtAttribute($value)
     {
-        date_default_timezone_set("America/Mexico");
+        date_default_timezone_set("America/Mexico_City");
         $this->attributes["created_at"] = Carbon::now();
     }
 
     public function setUpdatedAtAttribute($value)
     {
-        date_default_timezone_set("America/Mexico");
+        date_default_timezone_set('America/Mexico_City');
         $this->attributes["updated_at"] = Carbon::now();
     }
 
